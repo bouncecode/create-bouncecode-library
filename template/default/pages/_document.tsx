861ad0dd246e2a7f96da14be2622f4ff07bf96c7
Carl (Gyeongsik Park) <tpnet3@gmail.com>
2020-07-12 17:46:40 +0900

    Fix __html and update version

--- template/default/pages/_document.tsx
+++ template/default/pages/_document.tsx
@@ -35,20 +35,6 @@ class MyDocument extends Document {
             href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
             rel="stylesheet"
           />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,unsupported:true,api:2020.04 }; 
-              function $buo_f(){ 
-              var e = document.createElement("script"); 
-              e.src = "//browser-update.org/update.min.js"; 
-              document.body.appendChild(e);
-              };
-              try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-              catch(e){window.attachEvent("onload", $buo_f)}
-            `,
-            }}
-          />
         </Head>
         <body>
           <Main />
